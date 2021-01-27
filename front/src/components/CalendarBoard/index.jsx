17f625d1b3a7dce73b5145f5dd5c4f2242a57a63
nrwgkn <rinonkgwr@gmail.com>
2021-01-27 13:25:54 +0900

    refactor: ロジックを分離（src/serviceに移動）(2-4)

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,28 +1,8 @@
 import React from "react";
-import CalendarElement from "../CalendarElement";
 import { GridList } from "@material-ui/core";
+import CalendarElement from "../CalendarElement";
+import { createCalendar } from "../../services/calendar";
 import * as styles from "./style.css";
-
-import dayjs from "dayjs";
-import "dayjs/locale/ja";
-
-dayjs.locale("ja");
-
-const createCalendar = () => {
-  // 今月の最初の日を取得
-  const firstDay = dayjs().startOf("month");
-  // 最初の日の曜日の index を取得
-  const firstDayIndex = firstDay.day();
-
-  return Array(35)
-    .fill(0)
-    .map((_, i) => {
-      const diffFromFirstDay = i - firstDayIndex;
-      const day = firstDay.add(diffFromFirstDay, "day");
-
-      return day;
-    });
-};
 
 const calendar = createCalendar();
 
