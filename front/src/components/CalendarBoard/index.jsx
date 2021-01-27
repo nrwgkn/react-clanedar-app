56f4068a260175c1d96ac1905d2e3e7e9df1c57f
nrwgkn <rinonkgwr@gmail.com>
2021-01-27 13:36:08 +0900

    feat: 曜日を表示する(2-5)

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { GridList } from "@material-ui/core";
+import { GridList, Typography } from "@material-ui/core";
 import CalendarElement from "../CalendarElement";
 import { createCalendar } from "../../services/calendar";
 import * as styles from "./style.css";
+
+const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const calendar = createCalendar();
 
@@ -10,6 +12,19 @@ const CalendarBoard = () => {
   return (
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map((d) => (
+          <li key={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+        ))}
         {calendar.map((c) => (
           <li key={c.toISOString()}>
             <CalendarElement day={c} />
