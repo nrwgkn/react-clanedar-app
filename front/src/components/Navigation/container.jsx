fb025f714deaa60238c9999d8ae43c8bd7461e21
nrwgkn <rinonkgwr@gmail.com>
2021-01-27 18:01:49 +0900

    chore: コメント追加

--- front/src/components/Navigation/container.jsx
+++ front/src/components/Navigation/container.jsx
@@ -18,19 +18,22 @@ const mapDispatchToProps = (dispatch) => ({
 
 const mergeProps = (stateProps, dispatchProps) => ({
   // redux の state → dayjs に変換して props として渡す
+  // value: Datepicker の日付が現在表示されている日付に連動する（2021/02が表示されている状態で開くと2021/02が表示）
   month: getMonth(stateProps.calendar),
 
+  // onClick: 次月を表示
   setNextMonth: () => {
     const nextMonth = getNextMonth(stateProps.calendar);
     dispatchProps.setMonth(nextMonth);
   },
+  // onClick: 前月を表示
   setPreviousMonth: () => {
     const previousMonth = getPreviousMonth(stateProps.calendar);
     dispatchProps.setMonth(previousMonth);
   },
 
+  // onChange: 変更があったら dayjs → redux の state に変換して dispatch
   setMonth: (dayObj) => {
-    // 変更があったら dayjs → redux の state に変換して dispatch
     const month = formatMonth(dayObj);
     dispatchProps.setMonth(month);
   },
