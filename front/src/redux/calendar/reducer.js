40c05c599cb5c9ae1756c217eabc413720bfe0c6
nrwgkn <rinonkgwr@gmail.com>
2021-01-27 16:38:37 +0900

    refactor: 初期化部分をリファクタリング(3-4)

--- front/src/redux/calendar/reducer.js
+++ front/src/redux/calendar/reducer.js
@@ -1,13 +1,11 @@
 import dayjs from "dayjs";
 import { CALENDAR_SET_MONTH } from "./actions";
+import { formatMonth } from "../../services/calendar";
 
 const day = dayjs();
 
 // 初期値は今月のカレンダー
-const init = {
-  year: day.year(),
-  month: day.month() + 1,
-};
+const init = formatMonth(day);
 
 const calendarReducer = (state = init, action) => {
   const { type, payload } = action;
