6f5f57c6c00f73409b6e70d993fc1fcb7faa89fb
nrwgkn <rinonkgwr@gmail.com>
2021-01-27 17:38:51 +0900

    feat: DatePickerをカレンダー内で使えるようにする(3-5)

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
+import DayjsUtils from "@date-io/dayjs";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import rootReducer from "./redux/rootReducer";
 import CalendarBoard from "./components/CalendarBoard/container";
 import Navigation from "./components/Navigation/container";
@@ -15,8 +17,10 @@ dayjs.locale("ja");
 const App = () => (
   // connect しないとコンポーネントから参照することはできない
   <Provider store={store}>
-    <Navigation />
-    <CalendarBoard />
+    <MuiPickersUtilsProvider utils={DayjsUtils}>
+      <Navigation />
+      <CalendarBoard />
+    </MuiPickersUtilsProvider>
   </Provider>
 );
 
