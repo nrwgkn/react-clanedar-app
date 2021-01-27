5e8159d8511f5c7df7261f0655774229340755d8
nrwgkn <rinonkgwr@gmail.com>
2021-01-27 16:21:11 +0900

    feat: 作成したNavigationコンポーネントの呼び出しを追加(3-3)

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import CalendarBoard from "./components/CalendarBoard/container";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
 import rootReducer from "./redux/rootReducer";
+import CalendarBoard from "./components/CalendarBoard/container";
+import Navigation from "./components/Navigation/container";
 
 const store = createStore(rootReducer);
 
@@ -14,6 +15,7 @@ dayjs.locale("ja");
 const App = () => (
   // connect しないとコンポーネントから参照することはできない
   <Provider store={store}>
+    <Navigation />
     <CalendarBoard />
   </Provider>
 );
