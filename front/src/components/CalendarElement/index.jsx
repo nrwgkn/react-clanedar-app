995dc1357bb7b93415aacb2f94acca7cd22eb4ae
nrwgkn <rinonkgwr@gmail.com>
2021-01-27 13:40:11 +0900

    feat: 今日の日付のstyleを変更(2-6)

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import * as styles from "./style.css";
 import { Typography } from "@material-ui/core";
+import dayjs from "dayjs";
 
 const CalendarElement = ({ day }) => {
   const isFirstDay = day.date() === 1;
   const format = isFirstDay ? "M月D日" : "D";
+
+  const today = dayjs();
+  const compareFormat = "YYYYMMDD";
+  const isToday = day.format(compareFormat) === today.format(compareFormat);
 
   return (
     <div className={styles.element}>
@@ -14,7 +19,9 @@ const CalendarElement = ({ day }) => {
         variant="caption"
         component="div"
       >
-        {day.format(format)}
+        <span className={isToday ? styles.today : ""}>
+          {day.format(format)}
+        </span>
       </Typography>
     </div>
   );
