1d1bf4f1f1082a754dec9ee812111c26b5355d12
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 18:28:23 +0900

    feat: エラーの状態を管理するためのaction、reducerを実装(7-2)

--- front/src/redux/schedules/actions.js
+++ front/src/redux/schedules/actions.js
@@ -3,6 +3,8 @@ export const SCHEDULES_ADD_ITEM = "SCHEDULES_ADD_ITEM";
 export const SCHEDULES_FETCH_ITEM = "SCHEDULES_FETCH_ITEM";
 export const SCHEDULES_SET_LOADING = "SCHEDULES_SET_LOADING";
 export const SCHEDULES_DELETE_ITEM = "SCHEDULES_DELETE_ITEM";
+export const SCHEDULES_ASYNC_FAILURE = "SCHEDULES_ASYNC_FAILURE";
+export const SCHEDULES_RESET_ERROR = "SCHEDULES_RESET_ERROR";
 
 // actions
 export const schedulesAddItem = (payload) => ({
@@ -24,4 +26,13 @@ export const schedulesDeleteItem = (payload) => ({
   type: SCHEDULES_DELETE_ITEM,
   // 削除した schedule を排除した配列を payload として渡す
   payload,
+});
+
+export const schedulesAsyncFailure = (error) => ({
+  type: SCHEDULES_ASYNC_FAILURE,
+  error,
+});
+
+export const schedulesResetError = () => ({
+  type: SCHEDULES_RESET_ERROR,
 });
