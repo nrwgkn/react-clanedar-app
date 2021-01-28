802c9f67f86ab780b4773805810677927bb53f14
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 10:58:50 +0900

    feat: 入力データの受け渡しを定義(4-3)

--- front/src/components/AddScheduleDialog/presentation.jsx
+++ front/src/components/AddScheduleDialog/presentation.jsx
@@ -17,17 +17,36 @@ const Title = withStyles({
   root: { marginBottom: 32, fontSize: 22 },
 })(Input);
 
-const AddScheduleDialog = ({ schedule: { isDialogOpen }, closeDialog }) => {
+const AddScheduleDialog = ({
+  schedule: {
+    form: { title, location, description },
+    isDialogOpen,
+  },
+  closeDialog,
+  setSchedule,
+}) => {
   return (
     <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth="xs" fullWidth>
       <DialogContent>
-        <Title autoFocus fullWidth placeholder="タイトルと日時を追加" />
+        <Title
+          autoFocus
+          fullWidth
+          placeholder="タイトルと日時を追加"
+          value={title}
+          onChange={(e) => setSchedule({ title: e.target.value })}
+        />
         <Grid container spacing={1} alignItems="center" justify="space-between">
           <Grid item>
             <LocationOnOutlined />
           </Grid>
           <Grid item xs={10}>
-            <TextField style={spacer} fullWidth placeholder="場所を追加" />
+            <TextField
+              style={spacer}
+              fullWidth
+              placeholder="場所を追加"
+              value={location}
+              onChange={(e) => setSchedule({ location: e.target.value })}
+            />
           </Grid>
         </Grid>
         <Grid container spacing={1} alignItems="center" justify="space-between">
@@ -35,7 +54,13 @@ const AddScheduleDialog = ({ schedule: { isDialogOpen }, closeDialog }) => {
             <NotesOutlined />
           </Grid>
           <Grid item xs={10}>
-            <TextField style={spacer} fullWidth placeholder="説明を追加" />
+            <TextField
+              style={spacer}
+              fullWidth
+              placeholder="説明を追加"
+              value={description}
+              onChange={(e) => setSchedule({ description: e.target.value })}
+            />
           </Grid>
         </Grid>
       </DialogContent>
