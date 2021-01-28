2deae6b3dec3cb05ccc0e39971d97ff7bad6da98
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 15:23:34 +0900

    feat: 日付操作のためdate=>dayjsインスタンスに変換する関数を追加(6-3)

--- front/src/services/schedule.js
+++ front/src/services/schedule.js
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { isSameDay } from "./calendar";
 
 export const setSchedules = (calendar, schedules) =>
@@ -7,3 +8,9 @@ export const setSchedules = (calendar, schedules) =>
     // 引数として受け取った schedules のうち map されたそれぞれの calendar の日付に一致するものだけを filter してセット
     schedules: schedules.filter((e) => isSameDay(e.date, c)),
   }));
+
+export const formatSchedule = (schedule) => ({
+  ...schedule,
+  // date を dayjs インスタンスに変換
+  date: dayjs(schedule.date),
+});
