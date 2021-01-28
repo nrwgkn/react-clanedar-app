96690c2638dbff85e6b259cc1a09722792883ea0
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 10:14:15 +0900

    feat: AddScheduleDialogコンポーネントの作成(4-2)

---
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -0,0 +1,6 @@
+import AddScheduleDialog from "./presentation";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps)(AddScheduleDialog);
