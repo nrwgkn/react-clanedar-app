67eae940f6f77da1908d6977da86401501741254
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 10:35:31 +0900

    feat: ダイアログを閉じるための関数を実装(4-2)

--- front/src/components/AddScheduleDialog/container.jsx
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -1,6 +1,13 @@
-import AddScheduleDialog from "./presentation";
 import { connect } from "react-redux";
+import AddScheduleDialog from "./presentation";
+import { addScheduleCloseDialog } from "../../redux/addSchedule/actions";
 
 const mapStateToProps = (state) => ({ schedule: state.addSchedule });
 
-export default connect(mapStateToProps)(AddScheduleDialog);
+const mapDisapatchToProps = (dispatch) => ({
+  closeDialog: () => {
+    dispatch(addScheduleCloseDialog());
+  },
+});
+
+export default connect(mapStateToProps, mapDisapatchToProps)(AddScheduleDialog);
