6b78b6fae672f400166fbac524758dad3d0a736a
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 11:40:54 +0900

    feat: 追加された予定やサーバから取得した予定の管理のためのreducerを作成(5-1)

--- front/src/redux/schedules/reducer.js
+++ front/src/redux/schedules/reducer.js
@@ -1,4 +1,23 @@
+import { SCHEDULES_ADD_ITEM } from "./actions";
+
 const init = {
   items: [],
   isLoading: false,
 };
+
+const schedulesReducer = (state = init, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SCHEDULES_ADD_ITEM:
+      return {
+        ...state,
+        // 前回の items に payload として渡ってきた新規の予定を追加した配列を返
+        items: [...state.items, { ...payload, id: state.items.length + 1 }],
+      };
+    default:
+      return state;
+  }
+};
+
+export default schedulesReducer;
