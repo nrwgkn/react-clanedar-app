481baff9282f2ea0847cd1afa5db8fc32ab3f7fa
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 12:55:27 +0900

    feat: 作成したreducerをrootReducerに追加(5-5)

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import calendarReducer from "./calendar/reducer";
 import addScheduleReducer from "./addSchedule/reducer";
 import schedulesReducer from "./schedules/reducer";
+import currentScheduleReducer from "./currentSchedule/reducer";
 
 // {[state名]: [reducer]} で対応付け
 const rootReducer = combineReducers({
   calendar: calendarReducer,
   addSchedule: addScheduleReducer,
   schedules: schedulesReducer,
+  currentSchedule: currentScheduleReducer,
 });
 
 export default rootReducer;
