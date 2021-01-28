ed81da38cc0884d970bed4b441cbdd09b114d239
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 12:40:03 +0900

    feat: CalendarElementから作成したScheduleを呼び出せるようにする(5-4)

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -8,8 +8,9 @@ import {
   isSameDay,
   getMonth,
 } from "../../services/calendar";
+import Schedule from "../Schedule";
 
-const CalendarElement = ({ day, month }) => {
+const CalendarElement = ({ day, month, schedules }) => {
   // 今月以外をグレーダウン
   const currentMonth = getMonth(month);
   const isCurrentMonth = isSameMonth(day, currentMonth);
@@ -35,6 +36,11 @@ const CalendarElement = ({ day, month }) => {
           {day.format(format)}
         </span>
       </Typography>
+      <div className={styles.schedules}>
+        {schedules.map((e) => (
+          <Schedule key={e.id} schedule={e} />
+        ))}
+      </div>
     </div>
   );
 };
