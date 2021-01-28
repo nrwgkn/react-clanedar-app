37c4e5482918778a9e503469a3fc2da633d13aad
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 17:17:58 +0900

    feat: 予定を削除するためのactionを実装(6-5)

--- front/src/redux/schedules/actions.js
+++ front/src/redux/schedules/actions.js
@@ -1,8 +1,8 @@
 // constants
 export const SCHEDULES_ADD_ITEM = "SCHEDULES_ADD_ITEM";
-
 export const SCHEDULES_FETCH_ITEM = "SCHEDULES_FETCH_ITEM";
 export const SCHEDULES_SET_LOADING = "SCHEDULES_SET_LOADING";
+export const SCHEDULES_DELETE_ITEM = "SCHEDULES_DELETE_ITEM";
 
 // actions
 export const schedulesAddItem = (payload) => ({
@@ -18,4 +18,10 @@ export const schedulesFetchItem = (payload) => ({
 
 export const schedulesSetLoading = () => ({
   type: SCHEDULES_SET_LOADING,
+});
+
+export const schedulesDeleteItem = (payload) => ({
+  type: SCHEDULES_DELETE_ITEM,
+  // 削除した schedule を排除した配列を payload として渡す
+  payload,
 });
