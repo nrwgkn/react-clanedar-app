6cabd7e91442c249fe5f791a26c6ea1eae397680
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 18:25:37 +0900

    feat: エラーを捕捉する処理を必要(7-2)

--- front/src/redux/schedules/effects.js
+++ front/src/redux/schedules/effects.js
@@ -14,32 +14,41 @@ export const asyncSchedulesFetchItem = ({ month, year }) => async (
   // loading: true にする action を dispatch
   dispatch(schedulesSetLoading());
 
-  // 指定された月の予定を取得する API
-  // 非同期処理が終わるまで処理をブロック
-  // Promise の中身だけを result に格納
-  const result = await get(`schedules?month=${month}&year=${year}`);
+  // 非同期処理のエラーを捕捉できるように
+  try {
+    // 指定された月の予定を取得する API
+    // 非同期処理が終わるまで処理をブロック
+    // Promise の中身だけを result に格納
+    const result = await get(`schedules?month=${month}&year=${year}`);
 
-  // date はこのままの形だと日付の操作ができないため dayjs インスタンスに変換
-  // 新しい配列を返す
-  const formatedSchedule = result.map((r) => formatSchedule(r));
+    // date はこのままの形だと日付の操作ができないため dayjs インスタンスに変換
+    // 新しい配列を返す
+    const formatedSchedule = result.map((r) => formatSchedule(r));
 
-  // redux の状態として扱えるようになった formatedSchedule を dispatch
-  dispatch(schedulesFetchItem(formatedSchedule));
+    // redux の状態として扱えるようになった formatedSchedule を dispatch
+    dispatch(schedulesFetchItem(formatedSchedule));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const asyncSchedulesAddItem = (schedule) => async (dispatch) => {
   // loading: true にする
   dispatch(schedulesSetLoading());
 
-  // schedule を受け取り日付を ISOString という規格に変換
-  const body = { ...schedule, date: schedule.date.toISOString() };
-  // POST が成功すると追加された予定が返ってくる
-  const result = await post("schedules", body);
+  try {
+    // schedule を受け取り日付を ISOString という規格に変換
+    const body = { ...schedule, date: schedule.date.toISOString() };
+    // POST が成功すると追加された予定が返ってくる
+    const result = await post("schedules", body);
 
-  // date はこのままの形だと日付の操作ができないため dayjs インスタンスに変換
-  const newSchedule = formatSchedule(result);
-  // redux の状態として扱えるようになった newSchedule を dispatch
-  dispatch(schedulesAddItem(newSchedule));
+    // date はこのままの形だと日付の操作ができないため dayjs インスタンスに変換
+    const newSchedule = formatSchedule(result);
+    // redux の状態として扱えるようになった newSchedule を dispatch
+    dispatch(schedulesAddItem(newSchedule));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const asyncSchedulesDeleteItem = (id) => async (dispatch, getState) => {
@@ -48,10 +57,15 @@ export const asyncSchedulesDeleteItem = (id) => async (dispatch, getState) => {
   // getState(): thunk の関数の第二引数で store のデータを取得することができる
   const currentSchedules = getState().schedules.items;
 
-  // 削除の実行
-  await deleteRequest(`schedules/${id}`);
+  try {
+    await deleteRequest(`schedules/${id}`);
 
-  // 成功したらローカルの state を削除
-  const newSchedules = currentSchedules.filter((s) => s.id !== id);
-  dispatch(schedulesDeleteItem(newSchedules));
+    // 成功したらローカルの state を削除
+    const newSchedules = currentSchedules.filter((s) => s.id !== id);
+    dispatch(schedulesDeleteItem(newSchedules));
+  } catch (err) {
+    console.error(err);
+  }
+
+  // 削除の実行
 };
