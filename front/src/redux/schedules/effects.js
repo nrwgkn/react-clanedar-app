a922abba647504a32ffd4861e97136e9dcc77a4f
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 15:22:35 +0900

    feat: effects（非同期処理）を実装(6-3)

---
+++ front/src/redux/schedules/effects.js
@@ -0,0 +1,22 @@
+import { schedulesSetLoading, schedulesFetchItem } from "./actions";
+import { get } from "../../services/api";
+import { formatSchedule } from "../../services/schedule";
+
+// 非同期処理
+export const asyncSchedulesFetchItem = ({ month, year }) => async (
+  dispatch
+) => {
+  // loading: trueにする action を dispatch
+  dispatch(schedulesSetLoading());
+
+  // 指定された月の予定を取得する API
+  // 非同期処理が終わるまで処理をブロック
+  const result = await get(`schedules?month=${month}&year=${year}`);
+
+  // date はこのままの形だと日付の操作ができないため dayjs インスタンスに変換
+  // 新しい配列を返す
+  const formatedSchedule = result.map((r) => formatSchedule(r));
+
+  // redux の状態として扱えるようになった formatedSchedule を dispatch
+  dispatch(schedulesFetchItem(formatedSchedule));
+};
