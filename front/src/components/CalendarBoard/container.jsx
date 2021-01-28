404e0a5f5a988a639e4341929e360af5db893463
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 12:26:47 +0900

    feat: 作成した関数を用いてcalendarの生成方法を変更(5-3)

--- front/src/components/CalendarBoard/container.jsx
+++ front/src/components/CalendarBoard/container.jsx
@@ -2,6 +2,7 @@
 import { connect } from "react-redux";
 import CalendarBoard from "./presentation";
 import { createCalendar } from "../../services/calendar";
+import { setSchedules } from "../../services/schedule";
 import {
   addScheduleOpenDialog,
   addScheduleSetValue,
@@ -23,12 +24,20 @@ const mapDisapatchToProps = (dispatch) => ({
 // mergeProps: mapStateToProps で生成された props と mapDisapatchToProps で生成された props を引数にとり、コンポーネントで使う形に整形して渡す関数
 // mergeProps を独自に定義した場合は、自分で stateProps や dispatchProps を return してやらないと mergeProps で return したものしかコンポーネントに渡らない
 // mapStateToPropsの結果が前回と異なっていたときにだけ実行
-const mergeProps = (stateProps, dispatchProps) => ({
-  ...stateProps,
-  ...dispatchProps,
-  month: stateProps.calendar,
-  calendar: createCalendar(stateProps.calendar),
-});
+const mergeProps = (stateProps, dispatchProps) => {
+  const {
+    calendar: month,
+    schedules: { items: schedules },
+  } = stateProps;
+  const calendar = setSchedules(createCalendar(month), schedules);
+
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    calendar,
+    month,
+  };
+};
 
 export default connect(
   mapStateToProps,
