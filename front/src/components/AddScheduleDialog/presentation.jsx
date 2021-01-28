ff2b257937e64532366148d29d195801becb8cab
nrwgkn <rinonkgwr@gmail.com>
2021-01-28 10:35:56 +0900

    feat: 作成した関数をカレンダーから呼び出せるようにする(4-2)

--- front/src/components/AddScheduleDialog/presentation.jsx
+++ front/src/components/AddScheduleDialog/presentation.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Dialog, DialogContent } from "@material-ui/core";
 
-const AddScheduleDialog = ({ schedule: { isDialogOpen } }) => {
+const AddScheduleDialog = ({ schedule: { isDialogOpen }, closeDialog }) => {
   return (
-    <Dialog open={isDialogOpen} maxWidth="xs" fullWidth>
+    <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth="xs" fullWidth>
       <DialogContent>dialog</DialogContent>
     </Dialog>
   );
